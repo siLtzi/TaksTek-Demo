b62746fa5d05f0d955d7fde53876f2776cf4d641
Samuli Kuusisto <siltzuri@gmail.com>
2026-01-21 13:08:16 +0200

    chore

--- components/sections/Hero/index.tsx
+++ components/sections/Hero/index.tsx
@@ -1,5 +1,4 @@
 import { HeroSection as HeroSectionType } from '@/types/sanity'
-import { urlFor } from '@/sanity/lib/client'
 import HeroContent from './Content'
 
 // ============================================================================
@@ -16,23 +15,16 @@ export default function Hero({ section }: HeroProps) {
   const {
     heading,
     subheading,
-    backgroundImage,
     backgroundOverlay,
     primaryCta,
     secondaryCta,
     alignment = 'center',
   } = section
 
-  // Transform Sanity image to URL
-  const backgroundImageUrl = backgroundImage?.asset 
-    ? urlFor(backgroundImage.asset).width(1920).height(1080).url()
-    : undefined
-
   return (
     <HeroContent
       heading={heading}
       subheading={subheading}
-      backgroundImageUrl={backgroundImageUrl}
       backgroundOverlay={backgroundOverlay}
       primaryCta={primaryCta}
       secondaryCta={secondaryCta}
